c8c37cd525d97f7c93ca83797f9a29f8e559ca74
leao <lucasleaolima@gmail.com>
2024-04-16 17:20:58 -0300

    calculadora aprimorada

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,19 @@ import { reactive } from 'vue'
 import Calculadora from './components/Calculadora.vue'
 
 const state = reactive({
+  numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.'],
   number01: '',
   number02: '',
-  operation: 'soma',
-  dPorZero: 'Impossível dividir por 0'
+  operation: ['+', '-', '/', '*'],
+  simboloOperacao: '',
+  nomeOperacaoAtual: '',
+  numeroDaConta: '',
+  resultado: 0,
+  dPorZero: 'Impossível dividir por 0',
 })
 
 const divisaoPorZero = (n1, n2) => {
   const operacaoTeste = parseFloat(n1) / parseFloat(n2)
-  // console.log(operacaoTeste)
 
   if (isNaN(operacaoTeste)) {
     return '...'
@@ -23,9 +27,9 @@ const divisaoPorZero = (n1, n2) => {
 }
 
 const operacao = () => {
-  const { number01, number02, operation } = state
+  const { number01, number02, nomeOperacaoAtual } = state
 
-  switch (operation) {
+  switch (nomeOperacaoAtual) {
     case 'soma':
       return parseFloat(number01) + parseFloat(number02)
     case 'subtracao':
@@ -37,29 +41,142 @@ const operacao = () => {
   }
 }
 
-const fazerConta = (e) => {
-  const resultado = operacao()
 
-  if (resultado === state.dPorZero) {
-    return state.dPorZero
 
-  } else if (isNaN(resultado)) {
-    return '...'
-
-  } else {
-    return resultado
-  }
+const setNumber01 = (numero) => {
+  state.number01 = numero
 }
 
+const setNumber02 = (numero) => {
+  state.number02 = numero
+}
+
+const setNumbers = (evento) => {
+  state.numeroDaConta += evento.target.innerHTML
+  const listaNomesOp = ['soma', 'subtracao', 'multiplicacao', 'divisao']
+  
+  const teste = listaNomesOp.some(item => {
+    return item === state.nomeOperacaoAtual
+  })
+
+  if (teste) {
+    setNumber02(state.numeroDaConta)
+  } else {
+    setNumber01(state.numeroDaConta)
+  }
+
+}
+
+const resultadoZero = () => {
+  const verificacao = Boolean(state.number01 === '0' && !state.number02 && !state.nomeOperacaoAtual)
+  if (verificacao) {
+    state.numeroDaConta = ''
+  } else {
+    state.numeroDaConta = state.number01
+  }
+  return state.numeroDaConta
+}
+
+const setOperacao = (item) => {
+  let { number01, number02 } = state
+
+
+  const verificacao = number01.length >= 1 && number02.length === 0
+
+  if (verificacao) {
+    state.nomeOperacaoAtual = item.target.dataset.operacao
+    state.simboloOperacao = item.target.innerHTML
+    state.numeroDaConta = ''
+  } else if (!verificacao) {
+    // pass
+  }
+
+}
+
+const show = () => {
+  const resultadoTela = `${state.number01} ${state.simboloOperacao} ${state.number02}`
+  if (state.number01 === '') {
+    return '...'
+  } else {
+    return resultadoTela
+  }
+
+}
+
+const igual = (e) => {
+  state.resultado = operacao()
+  state.number01 = state.resultado.toString()
+  state.number02 = ''
+  state.simboloOperacao = ''
+  state.nomeOperacaoAtual = ''
+  resultadoZero()
+}
+
+const apagarElemento = () => {
+  const listaDeElementos = []
+
+  if (state.number02.length === 0 && state.simboloOperacao.length === 0) {
+    for (let i = 0; i < state.number01.length; i++) {
+      listaDeElementos.push(state.number01[i])
+    }
+    listaDeElementos.pop()
+
+    if (listaDeElementos.length === 0) {
+      state.numeroDaConta = ''
+      state.number01 = ''
+    } else {
+      state.numeroDaConta = ''
+      state.number01 = ''
+      listaDeElementos.forEach(itens => {
+        state.number01 += itens
+        state.numeroDaConta += itens
+      })
+    }
+  }
+
+  if (state.number02.length !== 0) {
+    for (let i = 0; i < state.number02.length; i++) {
+      listaDeElementos.push(state.number02[i])
+    }
+    listaDeElementos.pop()
+
+    if (listaDeElementos.length === 0) {
+      state.numeroDaConta = ''
+      state.number02 = ''
+    } else {
+      state.numeroDaConta = ''
+      state.number02 = ''
+      listaDeElementos.forEach(itens => {
+        state.number02 += itens
+        state.numeroDaConta += itens
+      })
+    }
+    console.log(state.number02.length)
+  } else {
+    state.nomeOperacaoAtual = ''
+    state.simboloOperacao = ''
+  }
+
+}
+
+const apagar = (evento) => {
+  apagarElemento()
+
+}
+
+
 </script>
+
 
 <template>
   <main class="main">
     <div class="container">
-      <Calculadora :fazer-conta="fazerConta()" :get-number01="item => state.number01 = item.target.value" :get-number02="item => state.number02 = item.target.value" :get-operation="item => state.operation = item.target.value" />
+      <Calculadora :get-numbers="state.numbers" :show="show()" :set-numbers="setNumbers" :set-operacao="setOperacao"
+        :igual="igual" :apagar="apagar" />
     </div>
   </main>
 </template>
+
 
 <style scoped>
 .main {
--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -1,32 +1,37 @@
 <script setup>
-const props = defineProps(['fazerConta', 'getNumber01', 'getNumber02', 'getOperation'])
+const props = defineProps(['igual', 'getNumbers', 'show', 'setNumbers', 'setOperacao', 'pegarElemento', 'apagar'])
+
 </script>
 
 <template>
     <div class="row">
-        <div class="col-md-5 ms-auto me-auto mt-5 d-flex justify-content-center">
-            <div class="calculadora p-3 rounded">
-                <p class="mostrador text-center fs-3 bg-white rounded"> {{ props.fazerConta }} </p>
-
-                <div class="numeros fs-3 ms-auto me-auto d-flex justify-content-center">
-                    <input @keyup="props.getNumber01" class="w-25" type="number">
-                    <select id="numeros" class="me-2 ms-2" @change="props.getOperation">
-                        <option value="soma">
-                            +
-                        </option>
-                        <option value="subtracao">
-                            -
-                        </option>
-                        <option value="divisao">
-                            /
-                        </option>
-                        <option value="multiplicacao">
-                            x
-                        </option>
-                    </select>
-                    <input @keyup="props.getNumber02" class="w-25" type="number">
+        <div class="col-5 ms-auto me-auto mt-5">
+            <div class="calculadora p-5">
+                <div class="row">
+                    <div class="col">
+                        <p class="mb-5 bg-light fs-2 text-center rounded">{{ props.show }}</p>
+                    </div>
                 </div>
-
+                <div class="row">
+                    <div class="numeros text-center">
+                        <ul class="list-group">
+                            <li @click="props.setNumbers" class="fs-2 list-group-item"
+                                v-for="numero in props.getNumbers">
+                                {{ numero }}
+                            </li>
+                            <li @click="props.setOperacao" class="fs-2 list-group-item" data-operacao="soma">+</li>
+                            <li @click="props.setOperacao" class="fs-2 list-group-item" data-operacao="subtracao">-</li>
+                            <li @click="props.setOperacao" class="fs-2 list-group-item" data-operacao="divisao">/</li>
+                            <li @click="props.setOperacao" class="fs-2 list-group-item" data-operacao="multiplicacao">*</li>
+                            <li @click="props.igual" class="fs-2 list-group-item">
+                                =
+                            </li>
+                            <li @click="props.apagar" class="fs-2 list-group-item">
+                                <<
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -35,5 +40,27 @@ const props = defineProps(['fazerConta', 'getNumber01', 'getNumber02', 'getOpera
 <style scoped>
 .calculadora {
     background-color: #3c6c88;
+    border-radius: 20px;
+
+    p {
+        border-radius: 20px;
+    }
+}
+
+.list-group {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 1rem;
+    place-items: center;
+
+    li {
+        width: 90px;
+        height: 90px;
+        cursor: pointer;
+        border-radius: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 }
 </style>
