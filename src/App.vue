53264f9da5ec5637058f1a161137be407567f6e9
leao <lucasleaolima@gmail.com>
2024-04-23 23:03:32 -0300

    projeto quase completamente terminado

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup>
 import { reactive } from 'vue'
 import Calculadora from './components/Calculadora.vue'
-
 const state = reactive({
-  numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.'],
+  numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, ','],
   number01: '',
   number02: '',
   simboloOperacao: '',
@@ -11,14 +10,16 @@ const state = reactive({
   numeroDaConta: '',
   resultado: 0,
   dPorZero: 'Impossível dividir por 0',
+  converterPonto: '',
+  number01Virgula: '',
+  number02Virgula: '',
+  juntarNumVirgula: ''
 })
 
 const divisaoPorZero = (n1, n2) => {
   const operacaoTeste = parseFloat(n1) / parseFloat(n2)
 
-  if (isNaN(operacaoTeste)) {
-    return '...'
-  } else if (n2 == 0) {
+  if (n2 == 0) {
     return state.dPorZero
   } else {
     return operacaoTeste
@@ -42,49 +43,107 @@ const operacao = () => {
   }
 }
 
-
-
+// Início dos SetNumbers
 const setNumber01 = (numero) => {
-  state.number01 = numero
+  state.number01Virgula = adicionarVirgula(numero.replace('.', ','))
+  state.number01 = numero.replace(',', '.')
 }
 
 const setNumber02 = (numero) => {
-  state.number02 = numero
+  state.number02Virgula = adicionarVirgula(numero.replace('.', ','))
+  state.number02 = numero.replace(',', '.')
+}
+
+const seMensagemDividirPorZero = () => {
+  if (state.numeroDaConta === state.dPorZero) {
+    state.numeroDaConta = ''
+  }
+}
+
+const apagarPontoDuplicado = () => {
+  const existe = -1
+  const jaTemPonto = state.numeroDaConta.indexOf(',') !== existe ? true : false
+  const doisPontos = state.numeroDaConta.indexOf(',,') !== existe ? true : false
+
+  if (jaTemPonto) {
+
+    if (doisPontos) {
+      state.numeroDaConta = state.numeroDaConta.replace(',,', ',')
+    }
+
+    const pontos = [...state.numeroDaConta].filter(item => {
+      return item === ','
+    })
+
+    if (pontos.length > 1) {
+      state.numeroDaConta = state.numeroDaConta.slice(0, state.numeroDaConta.lastIndexOf(','))
+    }
+  }
+
+}
+
+
+function adicionarVirgula(numero) {
+  let inde = numero.indexOf(',')
+  state.juntarNumVirgula = inde !== -1 ? numero.slice(inde) : ''
+  state.converterPonto = inde !== -1 ? numero.slice(0, inde) : numero
+
+  let resultado = '';
+
+  let tamanho = state.converterPonto.length;
+  let diferenca = tamanho % 3;
+
+  for (let i = 0; i < tamanho; i++) {
+    if (i > 0 && (i - diferenca) % 3 === 0) {
+      resultado += '.';
+    }
+    resultado += state.converterPonto[i];
+  }
+  return resultado + state.juntarNumVirgula
 }
 
 const setNumbers = (evento) => {
+  seMensagemDividirPorZero()
   state.numeroDaConta += evento.target.innerHTML
-  const listaNomesOp = ['soma', 'subtracao', 'multiplicacao', 'divisao', 'porcentagem']
-  if (state.numeroDaConta[0] === '.') {
+  apagarPontoDuplicado()
+
+
+  const seComecaComPonto = state.numeroDaConta[0] === ','
+
+  if (seComecaComPonto) {
     state.numeroDaConta = ''
   }
-  const teste = listaNomesOp.some(item => {
+
+  const listaNomesOp = ['soma', 'subtracao', 'multiplicacao', 'divisao', 'porcentagem']
+
+  const verificacao = listaNomesOp.some(item => {
     return item === state.nomeOperacaoAtual
   })
 
-  if (teste) {
+  if (verificacao) {
     setNumber02(state.numeroDaConta)
   } else {
     setNumber01(state.numeroDaConta)
   }
 
 }
+// Fim dos SetNumbers
 
 const resultadoZero = () => {
   const verificacao = Boolean(state.number01 === '0' && !state.number02 && !state.nomeOperacaoAtual)
+
   if (verificacao) {
     state.numeroDaConta = ''
   } else {
     state.numeroDaConta = state.number01
   }
-  return state.numeroDaConta
 }
 
 const setOperacao = (item) => {
   let { number01, number02 } = state
 
 
-  const verificacao = number01.length >= 1 && number02.length === 0
+  const verificacao = number01.length >= 1 && number02.length === 0 && state.number01 !== state.dPorZero
 
   if (verificacao) {
     state.nomeOperacaoAtual = item.target.dataset.operacao
@@ -97,8 +156,8 @@ const setOperacao = (item) => {
 }
 
 const show = () => {
-  const resultadoTela = `${state.number01} ${state.simboloOperacao} ${state.number02}`
-  if (state.number01 === '') {
+  const resultadoTela = `${state.number01Virgula} ${state.simboloOperacao} ${state.number02Virgula}`
+  if (state.number01Virgula === '') {
     return '...'
   } else {
     return resultadoTela
@@ -107,64 +166,106 @@ const show = () => {
 }
 
 const igual = (e) => {
+  const verificacao = Boolean(state.number02)
   state.resultado = operacao()
-  state.number01 = state.resultado.toString()
-  state.number02 = ''
-  state.simboloOperacao = ''
-  state.nomeOperacaoAtual = ''
-  resultadoZero()
+  if (verificacao) {
+    state.number01 = state.resultado.toString()
+    state.number02 = ''
+
+    const verificacaoDPorZero = state.resultado === state.dPorZero ? state.resultado : adicionarVirgula(state.number01.replace('.', ','))
+
+    state.number01Virgula = verificacaoDPorZero
+    state.number02Virgula = ''
+    state.simboloOperacao = ''
+    state.nomeOperacaoAtual = ''
+    resultadoZero()
+  }
+}
+
+const apagarElementoComVirgula = () => {
+  let listaDeElementosComVirgula = []
+
+  const verificacao = state.number02Virgula.length === 0 && state.simboloOperacao.length === 0
+  if (verificacao) {
+
+    listaDeElementosComVirgula = [...state.number01Virgula]
+    listaDeElementosComVirgula.pop()
+
+
+    if (listaDeElementosComVirgula.length === 0) {
+      state.number01Virgula = ''
+    } else {
+      state.number01Virgula = ''
+
+      listaDeElementosComVirgula.forEach(itens => {
+        state.number01Virgula += itens
+      })
+      state.number01Virgula = adicionarVirgula(state.number01Virgula.replace(/\./g, ''))
+
+    }
+  } else if (state.number02Virgula.length !== 0) {
+    listaDeElementosComVirgula = [...state.number02Virgula]
+    listaDeElementosComVirgula.pop()
+
+    if (listaDeElementosComVirgula.length === 0) {
+      state.number02Virgula = ''
+    } else {
+      state.number02Virgula = ''
+
+      listaDeElementosComVirgula.forEach(itens => {
+        state.number02Virgula += itens
+      })
+      state.number02Virgula = adicionarVirgula(state.number02Virgula.replace(/\./g, ''))
+    }
+  } else {
+    state.nomeOperacaoAtual = state.simboloOperacao = ''
+    state.numeroDaConta = state.number01
+  }
 }
 
 const apagarElemento = () => {
-  const listaDeElementos = []
+  let listaDeElementos = []
+  const verificacao = state.number02.length === 0 && state.simboloOperacao.length === 0
+  const impossivelDPorZero = state.number01 === state.dPorZero
+  if (verificacao) {
 
-  if (state.number02.length === 0 && state.simboloOperacao.length === 0) {
-    for (let i = 0; i < state.number01.length; i++) {
-      listaDeElementos.push(state.number01[i])
+    if (impossivelDPorZero) {
+      state.number01 = ''
+    } else {
+      listaDeElementos = [...state.number01]
+      listaDeElementos.pop()
+
+
+      if (listaDeElementos.length === 0) {
+        state.numeroDaConta = state.number01 = ''
+      } else {
+        state.numeroDaConta = state.number01 = ''
+
+        listaDeElementos.forEach(itens => {
+          state.numeroDaConta = state.number01 += itens
+        })
+
+      }
     }
+  } else if (state.number02.length !== 0) {
+    listaDeElementos = [...state.number02]
     listaDeElementos.pop()
 
     if (listaDeElementos.length === 0) {
-      state.numeroDaConta = ''
-      state.number01 = ''
+      state.numeroDaConta = state.number02 = ''
     } else {
-      state.numeroDaConta = ''
-      state.number01 = ''
+      state.numeroDaConta = state.number02 = ''
+
       listaDeElementos.forEach(itens => {
-        state.number01 += itens
-        state.numeroDaConta += itens
+        state.number02 = state.numeroDaConta += itens
       })
     }
   }
-
-  if (state.number02.length !== 0) {
-    for (let i = 0; i < state.number02.length; i++) {
-      listaDeElementos.push(state.number02[i])
-    }
-    listaDeElementos.pop()
-
-    if (listaDeElementos.length === 0) {
-      state.numeroDaConta = ''
-      state.number02 = ''
-    } else {
-      state.numeroDaConta = ''
-      state.number02 = ''
-      listaDeElementos.forEach(itens => {
-        state.number02 += itens
-        state.numeroDaConta += itens
-      })
-    }
-    console.log(state.number02.length)
-  } else {
-    state.nomeOperacaoAtual = ''
-    state.simboloOperacao = ''
-  }
-
 }
 
 const apagar = (evento) => {
   apagarElemento()
-
+  apagarElementoComVirgula()
 }
 
 
@@ -172,19 +273,9 @@ const apagar = (evento) => {
 
 
 <template>
-  <main class="main">
-    <div class="container">
-      <Calculadora :get-numbers="state.numbers" :show="show()" :set-numbers="setNumbers" :set-operacao="setOperacao"
-        :igual="igual" :apagar="apagar" />
-    </div>
-  </main>
+  <Calculadora :get-numbers="state.numbers" :show="show()" :set-numbers="setNumbers" :set-operacao="setOperacao"
+    :igual="igual" :apagar="apagar" />
 </template>
 
 
-<style scoped>
-.main {
-  height: 100vh;
-  width: 100vw;
-  background-color: #000;
-}
-</style>
+<style scoped></style>
--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -1,70 +1,54 @@
 <script setup>
+import { reactive } from 'vue';
+
 const props = defineProps(['igual', 'getNumbers', 'show', 'setNumbers', 'setOperacao', 'pegarElemento', 'apagar'])
+
+const state = reactive({
+    novoModo: ''
+})
+
+const mudarModo = (evento) => {
+    const modo = evento.target.checked
+    state.novoModo = modo
+}
 
 </script>
 
 <template>
-    <div class="row">
-        <div class="col-5 ms-auto me-auto mt-5">
-            <div class="calculadora p-5">
-                <div class="row">
-                    <div class="col">
-                        <p class="mb-5 bg-light fs-2 text-center rounded">{{ props.show }}</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="numeros text-center">
-                        <ul class="list-group">
-                            <li @click="props.setNumbers" class="fs-2 list-group-item"
-                                v-for="numero in props.getNumbers">
-                                {{ numero }}
-                            </li>
-                            <li @click="props.setOperacao" class="fs-2 list-group-item" data-operacao="soma">+</li>
-                            <li @click="props.setOperacao" class="fs-2 list-group-item" data-operacao="subtracao">-</li>
-                            <li @click="props.setOperacao" class="fs-2 list-group-item" data-operacao="divisao">/</li>
-                            <li @click="props.setOperacao" class="fs-2 list-group-item" data-operacao="multiplicacao">*</li>
-                            <li @click="props.setOperacao" class="fs-2 list-group-item" data-operacao="porcentagem">%</li>
-                            <li @click="props.igual" class="fs-2 list-group-item">
-                                =
-                            </li>
-                            <li @click="props.apagar" class="fs-2 list-group-item">
-                                <<
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+    <main class="main" :class="{'main--modo-light': state.novoModo}">
+        <div class="calculadora">
+            <p class="calculadora__display">{{ props.show }}</p>
+            <div class="numero">
+                <ul class="numero__list">
+                    <li class="numero__list__item" @click="props.setNumbers" v-for="numero in props.getNumbers">
+                        {{ numero }}
+                    </li>
+                    <li class="numero__list__item" @click="props.setOperacao" data-operacao="soma">+</li>
+                    <li class="numero__list__item" @click="props.setOperacao" data-operacao="subtracao">-</li>
+                    <li class="numero__list__item" @click="props.setOperacao" data-operacao="divisao">/</li>
+                    <li class="numero__list__item" @click="props.setOperacao" data-operacao="multiplicacao">*</li>
+                    <li class="numero__list__item" @click="props.setOperacao" data-operacao="porcentagem">%</li>
+                    <li class="igual numero__list__item" @click="props.igual">
+                        =
+                    </li>
+                    <li class="delete numero__list__item" @click="props.apagar">
+                        << </li>
+                </ul>
             </div>
         </div>
-    </div>
+        <div class="interruptor">
+            <input @change="mudarModo" class="check" id="check" type="checkbox">
+
+            <label class="sun-moon" for="check">
+                <i class="sun fa-solid fa-sun"></i>
+                
+                <i class="moon fa-solid fa-moon"></i>
+            </label>
+        </div>
+    </main>
+    
 </template>
 
-<style scoped>
-.calculadora {
-    background-color: #3c6c88;
-    border-radius: 20px;
-
-    p {
-        border-radius: 20px;
-    }
-}
-
-.list-group {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: .5rem;
-    place-items: center;
-
-    li {
-        width: 6rem;
-        height: 6rem;
-        max-width: 100%;
-        max-height: 100%;
-        cursor: pointer;
-        border-radius: 50px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-
-    }
-}
+<style lang="scss">
+@import '../scss/main.scss';
 </style>
